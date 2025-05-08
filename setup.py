50847a06cbd264ee617115bfe4e8dd88a49fbcfc
Eduardo Diaz <eduardoignaciodm@gmail.com>
2025-05-07 21:58:29 -0500

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Eduardo Diaz",                         # Tu nombre
+    author_email="eduardoignaciodm@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/EduardoIDiaz2000/TercerRepo",     # URL del proyecto
+)
